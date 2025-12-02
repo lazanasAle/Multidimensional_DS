d1b731f5b6ed49028aec99a91fb5768aa90e18ed
StamyKal <stamykalim@gmail.com>
2025-12-02 22:44:17 +0200

    Update ConvexHull_test.cpp
    Εχει γινει διορθωση στο polar sorting, προσθηκη tie breaker(ευκλειδια αποσταση). Μενει η διορθωση της συναρτησης για την δημιουργια του ConvexHull

--- ConvexHull_test.cpp
+++ ConvexHull_test.cpp
@@ -9,65 +9,94 @@ using namespace std;
 using namespace rapidcsv; //it needs downloading
 
 struct movie_point{
-double budget; //the x axis
-double popularity; //the y axis
+        double budget; //the x axis
+        double popularity; //the y axis
+
 };
 
 vector<movie_point> read_csv(string filename){
     vector<movie_point> points;
     Document doc("data_movies_clean.csv",LabelParams(0,-1));
-    
+
     vector<double> budgetV=doc.GetColumn<double>("budget");
     vector<double> popularityV=doc.GetColumn<double>("popularity");
-    
+
     for(auto i=0;i<budgetV.size();i++){
         movie_point point;
         point.budget=budgetV[i];
         point.popularity=popularityV[i];
-        points.push_back(point);  
-        
+        points.push_back(point);
+
     }
     return points;
-    
+
 }
 
-bool CompareBudget(const movie_point& a,const movie_point& b){
-return a.budget<b.budget;
+//counterclockwise function-->its corrected
+int  direction(movie_point p0,movie_point p1,movie_point p2){
+        double orientation=(p1.budget-p0.budget)*(p2.popularity-p0.popularity)-(p1.popularity-p0.popularity)*(p2.budget-p0.budget);
+        if(orientation>0) return 1;
+        else if (orientation<0) return -1;
+        else return 0;
+}
+//tie breaker
+double euclideian_dist(movie_point p0,movie_point p1){
+        double dist=(p0.budget-p1.budget)*(p0.budget-p1.budget) +(p0.popularity-p1.popularity)*(p0.popularity-p1.popularity);
+        return dist;
 }
 
-//counterclockwise function-->maybe i will convert to double
-double counterclockwise(movie_point p0,movie_point p1,movie_point p2){
-    double inprod1=p0.budget*p1.budget+p0.popularity*p1.popularity;
-    double inprod2=p0.budget*p2.budget+p0.popularity*p1.popularity;
-    return (inprod1*inprod2);
-}
-
-stack<movie_point> Create_Convex_hull(vector<movie_point> points){
-    stack<movie_point> S;
-    
-    S.push(points[0]);
-    S.push(points[1]);
-    S.push(points[2]);
-    
-    if(counterclockwise(points[0], points[1], points[2])<=0){
-        movie_point temp=S.top();
-        S.pop();
-        S.pop();
-        S.push(temp);
-    }
-    
-    for(auto i=3;i<points.size();i++){
-        S.push(points[i]);
-        if(counterclockwise(points[i-2], points[i-1], points[i])<=0){
-            movie_point temp=S.top();
-            S.pop();
-            S.pop();
-            S.push(temp);
+//it works
+vector<movie_point> polar_sorting(vector<movie_point> points){
+        sort(points.begin(), points.end(), [](const movie_point& a, const movie_point& b) {
+                return a.budget < b.budget;
+        });
+        movie_point pivot_point=points[0];
+        sort(points.begin(), points.end(),[&pivot_point](const movie_point& p1, const movie_point& p2){
+        int x=direction(pivot_point,p1,p2);
+        if(x==0){
+                return euclideian_dist(pivot_point,p1)<euclideian_dist(pivot_point,p2);
         }
-    
-    }
-    return S;
-    
+        return x>0;
+        });
+        return points;
+}
+
+
+//change that--> problemating
+// i want safety dont get to empty stack
+void Create_Convex_hull(vector<movie_point> points,stack<movie_point> S){
+
+
+        S.push(points[0]);
+        S.push(points[1]);
+        S.push(points[2]);
+
+
+        for(auto i=3;i<points.size();i++){
+                if(direction(points[i-2], points[i-1], points[i])>0){
+                        cout<<"counterclockwise 1"<<endl;
+                        S.push(points[i]);
+                }
+                else{
+                        cout<<"clockwise 1"<<endl;
+                        S.pop();
+                        int j=i-2;
+                        while(j>=2){ // want the begining
+                                if(direction(points[j-1], points[j], points[i])>0){
+                                        cout<<"counterclockwise 1.2"<<endl;
+                                        S.push(points[i]);
+                                        j=-1;//exit the loop
+                                }
+                                else{
+                                        cout<<"counterclockwise 1.2"<<endl;
+                                        S.pop();
+                                        j--;
+                                }
+                        }
+                }
+
+        }
+
 }
 
 int main(){
@@ -75,47 +104,19 @@ int main(){
     string filename="data_movies_clean.csv";
     vector<movie_point> points;
     points=read_csv(filename);
-    
-   
-    
-    //Sort the vector-->works
-    sort(points.begin(),points.end(),CompareBudget);      
-    
-    //create stack S and start the convex hull
-    stack<movie_point> S = Create_Convex_hull(points);
-    
-     
-    
-    
-    //take them csv to visualize
-    string filename1="export_test.csv";
-    vector<movie_point> hullPoints;
-    while (!S.empty()) {
-        hullPoints.push_back(S.top());
-        S.pop();
-    }
-    reverse(hullPoints.begin(), hullPoints.end());
 
-    
-    ofstream out(filename1);
-    
 
-    
-    out << "budget,popularity,is_hull\n";
 
-    
-    for (const auto& p : points) {
-        out << p.budget << "," << p.popularity << ",0\n";
-    }
+    points=polar_sorting(points);
 
-    
-    for (const auto& p : hullPoints) {
-        out << p.budget << "," << p.popularity << ",1\n";
-    }
 
-    out.close();
-    std::cout << "CSV file written to " << filename1 << "\n";
-    
-    
-    
+    //create stack S and start the convex hull-->problematic
+    stack<movie_point> S;
+    Create_Convex_hull(points,S);
+
+
+
+
+
+
 }

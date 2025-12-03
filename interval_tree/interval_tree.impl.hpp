7acac4c5a875595655912a443860c2aeb9cfde44
Eleni Siskou <helensiskou04@gmail.com>
2025-12-03 13:52:37 +0200

    Update interval_tree.impl.hpp
    forgot to add method for 'contains'
    
    runs ok as:
    g++ -std=c++17 test_interval_tree.cpp -o interval_test
    ./interval_test
    
     but still nothing is printed in the terminal cause it's not complete yet.

--- interval_tree/interval_tree.impl.hpp
+++ interval_tree/interval_tree.impl.hpp
@@ -13,6 +13,14 @@ bool interval<T>::overlaps(interval<T> &other)
 
 }
 
+template <typename T>
+bool interval<T>::contains(T point)
+{
+    return (point >= this->low && point <= this->high);
+
+}
+
+
 //will implement these here soon
        // void update_height(); //update the height of the tree based on the children' heights
        // void update_max_end(); //update max_end based on children and its own interal
@@ -20,3 +28,4 @@ bool interval<T>::overlaps(interval<T> &other)
        // void right_rotation();
        // void left_right_rotation();
        /// void right_left_rotation();
+// will also add comments to make it clear what each piece of code represents.

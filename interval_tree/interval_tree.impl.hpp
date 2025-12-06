169efb37f8ce6a46cdb64d1259fb1f5af75f4587
Eleni Siskou <helensiskou04@gmail.com>
2025-12-06 23:52:38 +0200

    left+right rotation
    implemented the left+right rotation. also added some semicolons i missed

--- interval_tree/interval_tree.impl.hpp
+++ interval_tree/interval_tree.impl.hpp
@@ -31,12 +31,11 @@ interval_node<T>::interval_node(interval<T> *i)
     this->inter=i;
 }
 
-
 template<typename T>
 int32_t interval_node<T>:: balance_factor()
 {
-    int32_t left_height=(this->left)? this->left->height:-1
-    int32_t right_height=(this->right)? this->right->height:-1
+    int32_t left_height=(this->left)? this->left->height:-1;
+    int32_t right_height=(this->right)? this->right->height:-1;
     return (left_height-right_height);
 
 
@@ -65,14 +64,95 @@ void interval_node<T>::update_max_end()
 template<typename T>
 void interval_node<T>::left_rotation()
 {
+    //safety check first (can't rotate left if there is no child to pivot in)
+    if(this->right ==nullptr)
+        return;
+    interval_node<T> *varA=this->left; //varA=the current left child
+    interval_node<T> *varB=this->right->left; //varB=the left child of the right node
+    interval_node<T> *varC=this->right->right; //varC=the right vhild of the right node
     
+    //saving the current node's interval data
+    interval<T> *tmp=this->inter; 
+
+    this->inter=this->right->inter; //overwrite the current node's data with the right child's data
+    //moves ups the right child without changing 'this' mem. adress
+    
+    this->left= new interval_node<T>(tmp); //node creation. it will act as the new left child
+    //it will contain the old root data
+
+    this->left->parent=this; //set the parent of this new node to be the current one
+    this->left->left=varA; //link again the subtrees
+
+    if(varA) //updates varA's parent pointer to the new lef node if varA exists
+    {
+        varA->parent=this->left;
+
+    }
+    //connect the middle subtree (varB) to be the right child of the new left node
+    this->left->right=varB; 
+
+    if(varB) //update varB' parent pointer if varB exists
+    {
+        varB->parent=this->left;
+
+    }
+    this->right=varC; //link again the subtrees
+    //we will attach the original right-right subtree -varC- to the current node's right
+
+    if(varC) //update varC's parent pointer to the current node (again, if varC exists)
+    {
+        varC->parent=this;
+    }
+
+    //now updating height+max_end for the new left child first
+    //it is lower in the tree
+    this->left->update_height();
+    this->left->update_max_end();
+
+    //update height+max_end for the current node (the new root)
+    this->update_height();
+    this->update_max_end();   
 }
 
-template<typename T>
-void interval_node<T>::right_rotation()
-{
-    
+//exact same logic with left rotation just above
+template <typename T>
+void interval_node<T>::right_rotation() {
+	if (this->left == nullptr)
+		return;
+	interval_node<T> *varA = this->left->left;
+	interval_node<T> *varB = this->left->right;
+	interval_node<T> *varC = this->right;
+
+	interval<T> *tmp = this->inter;
+	this->inter = this->left->inter;
+	this->right = new interval_node<T>(tmp);
+	this->right->parent = this;
+	this->left = varA;
+
+	if (varA)
+	{
+			varA->parent = this;
+	}
+	this->right->left = varB;
+	
+	if (varB)
+	{
+		varB->parent = this->right;
+	}
+	this->right->right = varC;
+
+	if (varC)
+	{	
+		varC->parent = this->right;
+	}
+
+	this->right->update_height();
+	this->right->update_max_end();
+	
+	this->update_height();
+	this->update_max_end();
 }
+
 
 template<typename T>
 void interval_node<T>::left_right_rotation()

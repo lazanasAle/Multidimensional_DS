55e7c73e45a87a35851cd3e403cec7ba07e18e2d
Alexis Lazanas <lazanas.ale@gmail.com>
2026-01-09 20:48:49 +0200

    moved time measurement to main

--- Sweep_line/Sweep_main.cpp
+++ Sweep_line/Sweep_main.cpp
@@ -1,5 +1,7 @@
 #include "Sweep_Line_impl.hpp"
 #include <print>
+#include <chrono>
+
 
 int main(int argc, char *argv[]) {
         string filename="Swip_line_Data.csv";
@@ -9,8 +11,13 @@ int main(int argc, char *argv[]) {
 
         event_queue eq = InitializeEvents(input_data.points, input_data.lines);
 
+        auto start = chrono::system_clock::now();
         set<point, PointComparator> is_points = Sweep_Line(eq);
+        auto end = chrono::system_clock::now();
+        chrono::duration<double> sweep_dur = end - start;
+        double sweep_time = sweep_dur.count();
 
+        println("execution time needed: {}", sweep_time);
         println("we have: {} intersection points", is_points.size());
         for (auto it = is_points.begin(); it != is_points.end(); ++it) {
                 println("Intersection point in: {} {}", it->x_axis, it->y_axis);
